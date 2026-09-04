1a6db80998e99dab9a0d9b581c6cb7539a1b6660
daniel.lozovan <daniel.lozovan@epitech.eu>
2026-09-04 14:17:27 +0200

    Feat: Use the the api of the back end

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -18,6 +18,16 @@ const nextConfig: NextConfig = {
    * fails while collecting page data.
    */
   outputFileTracingRoot: path.join(__dirname),
+
+  // 👉 Ajout du proxy silencieux pour le backend Flask
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
